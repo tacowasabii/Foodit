602d7e406df275c4f5f0883a073ba3f76e83ae95
tacowasabii <godseoril812@gmail.com>
2022-10-25 15:24:53 +0900

    App.js sort

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
-import FoodList from './FoodList';
-import items from '../mock.json';
+import { useState } from "react";
+import FoodList from "./FoodList";
+import items from "../mock.json";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
   return (
     <div>
-      <FoodList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} />
     </div>
   );
 }
