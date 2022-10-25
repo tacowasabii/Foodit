9b3b7f1793c9078cb06e8a8666da5b4107504bfb
tacowasabii <godseoril812@gmail.com>
2022-10-25 15:42:57 +0900

    add delete function

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import FoodList from "./FoodList";
-import items from "../mock.json";
+import mockitems from "../mock.json";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [items, setItems] = useState(mockitems);
   const handleNewestClick = () => setOrder("createdAt");
   const handleCalorieClick = () => setOrder("calorie");
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <FoodList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
