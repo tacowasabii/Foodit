2590d0fdac711cd5b4d2442f7f40adaf48657c93
tacowasabii <godseoril812@gmail.com>
2022-10-26 21:48:39 +0900

    add pagination function

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,47 @@
-import { getFoods } from "../api";
 import { useEffect, useState } from "react";
+import { getFoods } from "../api";
 import FoodList from "./FoodList";
-// import mockitems from "../mock.json";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [cursor, setCursor] = useState(null);
   const [items, setItems] = useState([]);
+
   const handleNewestClick = () => setOrder("createdAt");
+
   const handleCalorieClick = () => setOrder("calorie");
+
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-  const handleLoad = async (orderQuery) => {
-    const { foods } = await getFoods(orderQuery);
-    setItems(foods);
+
+  const handleLoad = async (options) => {
+    const {
+      foods,
+      paging: { nextCursor },
+    } = await getFoods(options);
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
   };
+
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+    });
+  };
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({
+      order,
+    });
   }, [order]);
 
   return (
@@ -27,6 +49,7 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      {cursor && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
