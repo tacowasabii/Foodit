2357ca15bb69b872defef98ba8e35a0c2dbcb201
tacowasabii <godseoril812@gmail.com>
2022-11-04 16:21:46 +0900

    add file clear function

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,10 +1,29 @@
+import { useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} />
+      <button type="button" onClick={handleClearClick}>
+        X
+      </button>
+    </div>
+  );
 }
 
 export default FileInput;
