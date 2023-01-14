8ba1687e54b1f8b17b3029178c8d9e5a7aa76dab
tacowasabii <godseoril812@gmail.com>
2023-01-14 17:27:22 +0900

    api로 데이터 불러오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
+import { getFoods } from "../api";
 import FoodList from "./FoodList";
-import mockItems from "../mock.json";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
 
   const handleNewestClick = () => setOrder("createdAt");
 
@@ -15,6 +15,11 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { foods } = await getFoods();
+    setItems(foods);
+  };
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   return (
@@ -22,6 +27,7 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
