8ef5278beb782e88a8e590f2c8a8d47fa8193672
tacowasabii <godseoril812@gmail.com>
2023-01-14 17:37:50 +0900

    페이지네이션 기능 구현

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-export async function getFoods(order = "") {
-  const query = `order=${order}`;
+export async function getFoods({ order = "", cursor = "", limit = 10 }) {
+  const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
   const response = await fetch(`https://learn.codeit.kr/api/foods?${query}`);
   const body = await response.json();
   return body;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import FoodList from "./FoodList";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [cursor, setCursor] = useState(null);
   const [items, setItems] = useState([]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -15,15 +16,32 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoad = async (orderQuery) => {
-    const { foods } = await getFoods(orderQuery);
-    setItems(foods);
+  const handleLoad = async (options) => {
+    const {
+      foods,
+      paging: { nextCursor },
+    } = await getFoods(options);
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+    });
   };
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({
+      order,
+    });
   }, [order]);
 
   return (
@@ -31,6 +49,7 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      {cursor && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
