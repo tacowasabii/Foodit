29cdd8f6ae3120d6324bc36c885de78146e184e1
tacowasabii <godseoril812@gmail.com>
2023-01-14 17:23:03 +0900

    데이터 목록 보여주기

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import FoodForm from "./FoodForm";
 import "./FoodList.css";
 
-function formatDate(value) {
-  const date = new Date(value);
-  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
-}
-
-function FoodListItem({ item, onEdit, onDelete }) {
-  const { imgUrl, title, calorie, content, createdAt } = item;
-
-  const handleEditClick = () => {
-    onEdit(item.id);
-  };
-
-  const handleDeleteClick = () => {
-    onDelete(item.id);
-  };
+function FoodListItem({ item }) {
+  const { imgUrl, title, calorie, content } = item;
 
   return (
     <div className="FoodListItem">
@@ -24,56 +9,18 @@ function FoodListItem({ item, onEdit, onDelete }) {
       <div>{title}</div>
       <div>{calorie}</div>
       <div>{content}</div>
-      <div>{formatDate(createdAt)}</div>
-      <button onClick={handleEditClick}>수정</button>
-      <button onClick={handleDeleteClick}>삭제</button>
     </div>
   );
 }
 
-function FoodList({ items, onUpdate, onUpdateSuccess, onDelete }) {
-  const [editingId, setEditingId] = useState(null);
-
-  const handleCancel = () => {
-    setEditingId(null);
-  };
-
+function FoodList({ items }) {
   return (
     <ul className="FoodList">
-      {items.map((item) => {
-        if (item.id === editingId) {
-          const { id, imgUrl, title, calorie, content } = item;
-          const initialValues = { title, calorie, content, imgFile: null };
-
-          const handleSubmit = (formData) => onUpdate(id, formData);
-
-          const handleSubmitSuccess = (newItem) => {
-            onUpdateSuccess(newItem);
-            setEditingId(null);
-          };
-
-          return (
-            <li key={item.id}>
-              <FoodForm
-                initialValues={initialValues}
-                initialPreview={imgUrl}
-                onSubmit={handleSubmit}
-                onSubmitSuccess={handleSubmitSuccess}
-                onCancel={handleCancel}
-              />
-            </li>
-          );
-        }
-        return (
-          <li key={item.id}>
-            <FoodListItem
-              item={item}
-              onEdit={setEditingId}
-              onDelete={onDelete}
-            />
-          </li>
-        );
-      })}
+      {items.map((item) => (
+        <li>
+          <FoodListItem item={item} />
+        </li>
+      ))}
     </ul>
   );
 }
