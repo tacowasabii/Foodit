858e9e7bb8744078b747b5ffb23a2fa4b3efa334
tacowasabii <godseoril812@gmail.com>
2023-01-14 17:24:52 +0900

    데이터 삭제 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import FoodList from "./FoodList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
+  const [items, setItems] = useState(mockItems);
 
   const handleNewestClick = () => setOrder("createdAt");
 
   const handleCalorieClick = () => setOrder("calorie");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -15,7 +21,7 @@ function App() {
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <FoodList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
