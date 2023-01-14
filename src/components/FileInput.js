8ca9aa62961b370ebe19ee35f16d7c8946bbf125
tacowasabii <godseoril812@gmail.com>
2023-01-14 17:51:01 +0900

    이미지 파일 미리보기

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,6 +1,7 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
   const inputRef = useRef();
 
   const handleChange = (e) => {
@@ -16,8 +17,20 @@ function FileInput({ name, value, onChange }) {
     onChange(name, null);
   };
 
+  useEffect(() => {
+    if (!value) return;
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview();
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]);
+
   return (
     <div>
+      <img src={preview} alt="이미지 미리보기" />
       <input type="file" onChange={handleChange} ref={inputRef} />
       <button type="button" onClick={handleClearClick}>
         X
